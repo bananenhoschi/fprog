4ffb9b111e77ed2d3794e6b865e6e505d0688e82
Carlo Baumann <carlo.baumann@students.fhnw.ch>
2018-11-03 18:20:30 +0100

    Worksheet Lektion 3 1. Listen

---
+++ Worksheets/3.1_Listen.hs
@@ -0,0 +1,15 @@
+-- Börsenkurse
+[(String, Double)]
+[("GOOG", 875.92), ("MSFT", 32.85), ("ORCL", 33.75)]
+
+-- Einkaufsliste
+[(String, Int)]
+[("Banane", 2), ("Schokolade", 3), ("Eier",4)]
+
+-- Module
+[(Int, [String])]
+[(1, ["oop1","dist","mada"]), (2, ["depa", "oop2", "fprog"])]
+
+-- Videothek
+[(String, String, Bool)]
+[("Max", "Star Wars", False), ("Moritz", "Frozen", True), ("Alice", "Pretty Woman", False)]
